637fa803edccb4d73f679111d9935aca8f6f01e1
Ryan Frishkorn <ryan@bluerift.io>
2023-07-03 00:30:32 -0700

    Parse DateTime upon query

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use chrono;
+use chrono::{DateTime, FixedOffset};
 use rusqlite::{Connection, Result};
 use rust_stemmers::{Algorithm, Stemmer};
 use std::error::Error;
 use std::{env, io};
 use uuid::Uuid;
 
+#[allow(dead_code)]
+#[derive(Debug)]
 struct Snip {
     uuid: String,
     name: String,
-    timestamp: String,
+    text: String,
+    timestamp: DateTime<FixedOffset>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -21,32 +24,84 @@ fn main() -> Result<(), Box<dyn Error>> {
     let db_path = env::var("SNIP_DB").unwrap_or(format!("{}/{}", home_dir, db_file_default));
     let conn = Connection::open(db_path)?;
 
-    let mut stmt = match conn.prepare("SELECT uuid, name, timestamp from snip") {
+    match list_snips(&conn) {
+        Ok(_) => (),
+        Err(e) => panic!("{}", e),
+    }
+
+    let word = read_data_from_stdin()?.trim_end().to_string();
+    let stem = stem_something(&word);
+    println!("Stem word: {} -> {}", word, stem);
+    let s_first = match get_first_snip(&conn) {
+        Ok(v) => v,
+        Err(e) => panic!("{}", e),
+    };
+    println!("first snip: {:?}", s_first);
+
+    Ok(())
+}
+
+fn get_first_snip(conn: &Connection) -> Result<Snip, Box<dyn Error>> {
+    let mut stmt = match conn.prepare("SELECT uuid, name, timestamp, data FROM snip LIMIT 1") {
+        Ok(v) => v,
+        Err(e) => return Err(Box::new(e)),
+    };
+
+    let mut query_iter = stmt.query_map([], |row| {
+        // parse timestamp
+        let ts: String = row.get(2)?;
+        let ts_parsed = match DateTime::parse_from_rfc3339(ts.as_str()) {
+            Ok(v) => v,
+            Err(e) => panic!("{}", e),
+        };
+
+        Ok(Snip {
+            uuid: row.get(0)?,
+            name: row.get(1)?,
+            timestamp: ts_parsed,
+            text: row.get(3)?,
+        })
+    })?;
+
+    if let Some(s) = query_iter.next() {
+        return Ok(s.unwrap());
+    }
+
+    Err(Box::new(std::io::Error::new(
+        io::ErrorKind::NotFound,
+        "damn",
+    )))
+}
+
+fn list_snips(conn: &Connection) -> Result<(), Box<dyn Error>> {
+    let mut stmt = match conn.prepare("SELECT uuid, name, timestamp, data from snip") {
         Ok(v) => v,
         Err(e) => panic!("{}", e),
     };
 
     let query_iter = stmt.query_map([], |row| {
+        // parse timestamp
+        let ts: String = row.get(2)?;
+        let ts_parsed = match DateTime::parse_from_rfc3339(ts.as_str()) {
+            Ok(v) => v,
+            Err(e) => panic!("{}", e),
+        };
+
         Ok(Snip {
             uuid: row.get(0)?,
             name: row.get(1)?,
-            timestamp: row.get(2)?,
+            timestamp: ts_parsed,
+            text: row.get(3)?,
         })
     })?;
 
     for snip in query_iter {
         let s = snip.unwrap();
-        let id = match Uuid::parse_str(&s.uuid) {
-            Ok(v) => v,
-            Err(e) => panic!("{}", e),
-        };
+        let id = Uuid::parse_str(&s.uuid)?;
+
         print!("{} - ", chrono::Utc::now());
         println!("{} {} {}", split_uuid(id)[0], s.timestamp, s.name);
     }
-
-    let word = read_data_from_stdin()?.trim_end().to_string();
-    let stem = stem_something(&word);
-    print!("Stem word: {} -> {}\n", word, stem);
 
     Ok(())
 }
