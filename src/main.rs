1667197ee5d89ee5e9f770af5ed93d569e4751de
Ryan Frishkorn <ryan@bluerift.io>
2023-07-02 22:13:09 -0700

    Add uuid, time, stemmer libs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use chrono;
 use rusqlite::{Connection, Result};
+use rust_stemmers::{Algorithm, Stemmer};
 use std::error::Error;
-use std::io;
+use std::{env, io};
+use uuid::Uuid;
 
 struct Snip {
     uuid: String,
@@ -9,14 +12,21 @@ struct Snip {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let conn = Connection::open("snip.sqlite3")?;
+    let db_file_default = ".snip.sqlite3".to_string();
+
+    let home_dir = match env::var("HOME") {
+        Ok(v) => v,
+        Err(e) => panic!("{}", e),
+    };
+    let db_path = env::var("SNIP_DB").unwrap_or(format!("{}/{}", home_dir, db_file_default));
+    let conn = Connection::open(db_path)?;
 
     let mut stmt = match conn.prepare("SELECT uuid, name, timestamp from snip") {
         Ok(v) => v,
         Err(e) => panic!("{}", e),
     };
 
-    let snip_iter = stmt.query_map([], |row| {
+    let query_iter = stmt.query_map([], |row| {
         Ok(Snip {
             uuid: row.get(0)?,
             name: row.get(1)?,
@@ -24,19 +34,35 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
     })?;
 
-    for snip in snip_iter {
+    for snip in query_iter {
         let s = snip.unwrap();
-        let uuid_split = split_uuid(s.uuid)?;
-        println!("{} {} {}", uuid_split[0], s.timestamp, s.name);
+        let id = match Uuid::parse_str(&s.uuid) {
+            Ok(v) => v,
+            Err(e) => panic!("{}", e),
+        };
+        print!("{} - ", chrono::Utc::now());
+        println!("{} {} {}", split_uuid(id)[0], s.timestamp, s.name);
     }
+
+    let word = read_data_from_stdin()?.trim_end().to_string();
+    let stem = stem_something(&word);
+    print!("Stem word: {} -> {}\n", word, stem);
 
     Ok(())
 }
 
-fn split_uuid(uuid: String) -> Result<Vec<String>, io::Error> {
-    let parts: Vec<String> = uuid.split("-").map(|s| s.to_string()).collect();
-    match parts.len() {
-        5 => return Ok(parts),
-        _ => return Err(io::Error::new(io::ErrorKind::Other, "error parsing uuid")),
-    }
+fn stem_something(s: &str) -> String {
+    let stemmer = Stemmer::create(Algorithm::English);
+    stemmer.stem(s.to_lowercase().as_str()).to_string()
+}
+
+#[allow(dead_code)]
+fn read_data_from_stdin() -> Result<String, io::Error> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn split_uuid(uuid: Uuid) -> Vec<String> {
+    uuid.to_string().split('-').map(|s| s.to_string()).collect()
 }
