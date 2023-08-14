dded2d2d629f3a2e2b76cfc8d3ca775e763dc23d
Ryan Frishkorn <ryan@bluerift.io>
2023-08-14 15:10:55 -0700

    Add author information

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+/*
+Snip - a personal information storage and search tool
+Copyright (C) 2023, Ryan Frishkorn
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 pub mod snip;
 
-use atty;
 use crate::snip::{SearchMethod, SearchQuery, Snip, SnipAnalysis, SnipError};
 use clap::{Arg, ArgAction, Command};
 use colored::*;
