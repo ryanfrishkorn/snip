ec47a99a3e0e4e996b7bc09c1fcb497dd38949a5
Ryan Frishkorn <ryan@bluerift.io>
2025-04-06 16:08:29 -0400

    Remove use statements

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,3 @@ pub mod doc;
 pub mod error;
 pub mod search;
 pub mod test_prep;
-
-/*
-pub use analysis::*;
-pub use attachment::*;
-pub use doc::*;
-pub use error::SnipError;
-pub use search::*;
-pub use test_prep::*;
-*/
